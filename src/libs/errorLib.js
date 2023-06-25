28d6da7d87a8e20a451fcb6d9a4a5e26d97fa616
Avishek3223 <avishekmishra56@gmail.com>
2023-06-25 07:15:13 +0530

    add error

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,62 +1,47 @@
 import * as Sentry from "@sentry/browser";
-
-const isLocal = false;
-
+const isLocal = process.env.NODE_ENV === "development";
 export function initSentry() {
-  if (isLocal) {
-    return;
-  }
-
-  Sentry.init({
-    dsn: "https://45cff322dba84342a7fe02d22b8143ec@o4505417339502592.ingest.sentry.io/4505417346187264",
-    integrations: [
-      new Sentry.BrowserTracing({
-        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: ["localhost"],
-      }),
-      new Sentry.Replay(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  });
-
-export function logError(error, errorInfo = null) {
-  if (isLocal) {
-    return;
-  }
-
-  Sentry.withScope((scope) => {
-    if (errorInfo) {
-      scope.setExtras(errorInfo);
+    if (isLocal) {
+        return;
     }
-    Sentry.captureException(error);
-  });
-}
+    Sentry.init({
+        dsn: "https://45cff322dba84342a7fe02d22b8143ec@o4505417339502592.ingest.sentry.io/4505417346187264",
+        integrations: [
+            new Sentry.BrowserTracing({
+                // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+                tracePropagationTargets: ["localhost", /^(http:\/\/)?localhost:3000\//],
+            }),
+            new Sentry.Replay(),
+        ],
+        // Performance Monitoring
+        tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+        // Session Replay
+        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    });
 
-export function onError(error) {
-  let errorInfo = {};
-  let message = error.toString();
-
-  if (!(error instanceof Error) && error.message) {
-    errorInfo = error;
-    message = error.message;
-    error = new Error(message);
-  } else if (error.config && error.config.url) {
-    errorInfo.url = error.config.url;
-  }
-
-  logError(error, errorInfo);
-  alert(message);
-}
-
-// Additional function for capturing custom messages to Sentry
-export function logMessage(message, level = "info") {
-  if (isLocal) {
-    return;
-  }
-
-  Sentry.captureMessage(message, level);
+    export function logError(error, errorInfo = null) {
+        if (isLocal) {
+            return;
+        }
+        Sentry.withScope((scope) => {
+            errorInfo && scope.setExtras(errorInfo);
+            Sentry.captureException(error);
+        });
+    }
+    export function onError(error) {
+        let errorInfo = {};
+        let message = error.toString();
+        // Auth errors
+        if (!(error instanceof Error) && error.message) {
+            errorInfo = error;
+            message = error.message;
+            error = new Error(message);
+            // API errors
+        } else if (error.config && error.config.url) {
+            errorInfo.url = error.config.url;
+        }
+        logError(error, errorInfo);
+        alert(message);
+    } 
 }

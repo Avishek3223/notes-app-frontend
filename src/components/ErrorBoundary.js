46b148593f3a59c5742eb7326e744ac39cf6fdca
Avishek3223 <avishekmishra56@gmail.com>
2023-06-25 06:45:08 +0530

    some changes

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { logError } from "../libs/errorLib";
 import "./ErrorBoundary.css";
+
 export default class ErrorBoundary extends React.Component {
-    state = { hasError: false };
-    static getDerivedStateFromError(error) {
-        return { hasError: true };
-    }
-    componentDidCatch(error, errorInfo) {
-        logError(error, errorInfo);
-    }
-    render() {
-        return this.state.hasError ? (
-            <div className="ErrorBoundary text-center">
-                <h3>Sorry there was a problem loading this page</h3>
-            </div>
-        ) : (
-            this.props.children
-        );
-    }
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    logError(error, errorInfo);
+  }
+
+  triggerError = () => {
+    throw new Error("Manually triggered error");
+  };
+
+  render() {
+    return this.state.hasError ? (
+      <div className="ErrorBoundary text-center">
+        <h3>Sorry, there was a problem loading this page</h3>
+      </div>
+    ) : (
+      <div className="ErrorBoundary">
+        <h1>Wrapped Component</h1>
+        <button onClick={this.triggerError}>Trigger Error</button>
+        {this.props.children}
+      </div>
+    );
+  }
 }

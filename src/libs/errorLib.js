c7634673e6f318ac3bdd6a9dc565ba1d2a78890b
Avishek3223 <avishekmishra56@gmail.com>
2023-06-25 07:18:36 +0530

    /

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,9 +1,11 @@
 import * as Sentry from "@sentry/browser";
 const isLocal = process.env.NODE_ENV === "development";
+
 export function initSentry() {
     if (isLocal) {
         return;
     }
+
     Sentry.init({
         dsn: "https://45cff322dba84342a7fe02d22b8143ec@o4505417339502592.ingest.sentry.io/4505417346187264",
         integrations: [
@@ -19,29 +21,34 @@ export function initSentry() {
         replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
         replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
     });
+}
 
-    export function logError(error, errorInfo = null) {
-        if (isLocal) {
-            return;
-        }
-        Sentry.withScope((scope) => {
-            errorInfo && scope.setExtras(errorInfo);
-            Sentry.captureException(error);
-        });
+export function logError(error, errorInfo = null) {
+    if (isLocal) {
+        return;
     }
-    export function onError(error) {
-        let errorInfo = {};
-        let message = error.toString();
-        // Auth errors
-        if (!(error instanceof Error) && error.message) {
-            errorInfo = error;
-            message = error.message;
-            error = new Error(message);
-            // API errors
-        } else if (error.config && error.config.url) {
-            errorInfo.url = error.config.url;
-        }
-        logError(error, errorInfo);
-        alert(message);
-    } 
+
+    Sentry.withScope((scope) => {
+        errorInfo && scope.setExtras(errorInfo);
+        Sentry.captureException(error);
+    });
+}
+
+export function onError(error) {
+    let errorInfo = {};
+    let message = error.toString();
+
+    // Auth errors
+    if (!(error instanceof Error) && error.message) {
+        errorInfo = error;
+        message = error.message;
+        error = new Error(message);
+    }
+    // API errors
+    else if (error.config && error.config.url) {
+        errorInfo.url = error.config.url;
+    }
+
+    logError(error, errorInfo);
+    alert(message);
 }
